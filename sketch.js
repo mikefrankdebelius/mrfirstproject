782c1421873d0cfe47f7005650a0bc6d6cba96c7
mikefrankdebelius <mikefrankdebelius@gmail.com>
2016-10-07 23:53:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {  createCanvas(1000,1000);
 
 }
 
-function draw() { background(0,0,0);
+function draw() { background(0,0,0,50);
                  ellipse(mouseX,mouseY,100,100);
 
 }
